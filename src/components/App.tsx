5b6a6ca7e08c11e6cba730e8e9c1150480848d67
Charles Liu <liucharlesche@gmail.com>
2020-03-31 18:25:42 +0900

    added newly crafted components state variables into main App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Inbox from './Inbox';
+import ResultDisplay from './ResultDisplay';
+import DateRangeBar from './DateRangeBar';
 
 import '../styles/index.scss';
 
-class App extends React.PureComponent {
-    render() {
-        return (
-            <div>
-                <h1>Wassssaaaaaaap</h1>
-            </div>
-        );
-    }
-}
+const App: React.FC = () => {
+    const [dateRange, setDateRange] = useState({ startDate: new Date('2019/12/31'), endDate: new Date('2020/1/3'));
+    const [emailCount, setEmailCount] = useState(0);
+    return (
+        <div className="app">
+            <DateRangeBar dateRange={dateRange}  setDateRange={setDateRange} />
+            <ResultDisplay emailCount={emailCount} />
+            <hr />
+            <Inbox emailCount={emailCount} setEmailCount={setEmailCount} dateRange={dateRange} />
+        </div>
+    );
+};
 
 export default App;
