4b672459c821dac79047cea645565273a786f84c
Charles Liu <liucharlesche@gmail.com>
2020-03-31 22:31:17 +0900

    minor changes in app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,15 +7,14 @@ import DateRangeBar from './DateRangeBar';
 import '../styles/index.scss';
 
 const App: React.FC = () => {
-    const [dateRange, setDateRange] = useState({ startDate: new Date('2019/12/31'), endDate: new Date('2020/1/3'));
+    const [dateRange, setDateRange] = useState({ startDate: new Date('2019/12/31'), endDate: new Date('2020/1/3') });
     const [emailCount, setEmailCount] = useState(0);
     return (
-        <div className="app">
-            <DateRangeBar dateRange={dateRange}  setDateRange={setDateRange} />
+        <>
+            <DateRangeBar dateRange={dateRange} setDateRange={setDateRange} />
             <ResultDisplay emailCount={emailCount} />
-            <hr />
             <Inbox emailCount={emailCount} setEmailCount={setEmailCount} dateRange={dateRange} />
-        </div>
+        </>
     );
 };
 
